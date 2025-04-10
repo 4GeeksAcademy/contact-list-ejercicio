7a5b453496a21b8401a2e3311f04d221ea889d7c
rvarga09 <146484780+rvarga09@users.noreply.github.com>
2025-04-10 21:03:14 +0000

    contact list ejercicio 1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,24 @@
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
+import ContactCard from "../components/ContactCard.jsx";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
 export const Home = () => {
 
-  const {store, dispatch} =useGlobalReducer()
+	const { store, dispatch } = useGlobalReducer()
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
+			<h1>Agenda</h1>
+			<div className= "d-flex flex-column align-items-center">
+
+				
+				<ContactCard/>
+				<ContactCard/>
+				<ContactCard/>
+				<ContactCard/>
+
+			</div>
+
 		</div>
 	);
 }; 
