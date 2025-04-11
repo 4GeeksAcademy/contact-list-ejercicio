f8a10fde2a9ad5def8d90eae85994ce13695086d
rvarga09 <146484780+rvarga09@users.noreply.github.com>
2025-04-11 21:14:18 +0000

    contac list ejercicio 2

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,9 +1,22 @@
 function ContactCard() {
     return (
-        <div className="card" style={{ width: "18rem" }}>
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <div className="card mb-3 p-3" style={{ maxWidth: "600px" }}>
+            <div className="row g-0">
+                <div className="col-md-3">
+                    <img src="https://avatar.iran.liara.run/public" className="img-fluid rounded-start" alt="..." />
+                </div>
+                <div className="col-md-6">
+                    <div className="card-body text-start">
+                        <h5 className="card-title">Rafael Vargas</h5>
+                        <p className="card-text"><i class="fa-solid fa-location-dot me-2"></i> Direccion</p>
+                        <p className="card-text"><i class="fa-solid fa-phone me-2"></i> Telefono</p>
+                        <p className="card-text"><i class="fa-solid fa-envelope me-2"></i> Correo</p>
+                    </div>
+                </div>
+                <div className="col-md-3 d-flex justify-content-center align-items-center gap-1">
+                    <button className="btn btn-primary">Editar</button>
+                    <button className="btn btn-danger">Borrar</button>
+                </div>
             </div>
         </div>
     )
