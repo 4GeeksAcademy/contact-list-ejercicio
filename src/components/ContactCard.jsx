3f754a1479c3b53741f04d1ddd5524c490f36b20
rvarga09 <146484780+rvarga09@users.noreply.github.com>
2025-04-12 22:08:26 +0000

    contact list 3

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,4 +1,4 @@
-function ContactCard() {
+function ContactCard({contact}) {
     return (
         <div className="card mb-3 p-3" style={{ maxWidth: "600px" }}>
             <div className="row g-0">
@@ -7,15 +7,15 @@ function ContactCard() {
                 </div>
                 <div className="col-md-6">
                     <div className="card-body text-start">
-                        <h5 className="card-title">Rafael Vargas</h5>
-                        <p className="card-text"><i class="fa-solid fa-location-dot me-2"></i> Direccion</p>
-                        <p className="card-text"><i class="fa-solid fa-phone me-2"></i> Telefono</p>
-                        <p className="card-text"><i class="fa-solid fa-envelope me-2"></i> Correo</p>
+                        <h5 className="card-title">{contact.name}</h5>
+                        <p className="card-text"><i class="fa-solid fa-location-dot me-2"></i> {contact.direction}</p>
+                        <p className="card-text"><i class="fa-solid fa-phone me-2"></i> {contact.phone}</p>
+                        <p className="card-text"><i class="fa-solid fa-envelope me-2"></i> {contact.email}</p>
                     </div>
                 </div>
                 <div className="col-md-3 d-flex justify-content-center align-items-center gap-1">
-                    <button className="btn btn-primary">Editar</button>
-                    <button className="btn btn-danger">Borrar</button>
+                    <button className="btn btn-primary"> <i class="fa-solid fa-pen-to-square"></i>Editar</button>
+                    <button className="btn btn-danger"> <i class="fa-solid fa-trash-can"></i>Borrar</button>
                 </div>
             </div>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,29 @@ import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import ContactCard from "../components/ContactCard.jsx";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
+let contactList = [
+	{
+		id: 1,
+		name: "Rafael Vargas",
+		direction: "caracas, Venezuela",
+		phone: "0416-1234567",
+		email: "rafael@gmail.com"
+	},
+	{
+		id: 2,
+		name: "Maria Vargas",
+		direction: "Caracas, Venezuela",
+		phone: "0412-3456789",
+		email: "maria24@gmail.com"
+	},
+	{
+		id: 3,
+		name: "Jose Vargas",
+		direction: "Caracas, Venezuela",
+		phone: "0412-9876543",
+		email: "lucciano@gmail.com"
+	}
+];
 export const Home = () => {
 
 	const { store, dispatch } = useGlobalReducer()
@@ -12,10 +35,10 @@ export const Home = () => {
 			<div className= "d-flex flex-column align-items-center">
 
 				
-				<ContactCard/>
-				<ContactCard/>
-				<ContactCard/>
-				<ContactCard/>
+				<ContactCard contact={contactList[0]} />
+				<ContactCard contact={contactList[1]} />
+				<ContactCard contact={contactList[2]} />
+				
 
 			</div>
 
