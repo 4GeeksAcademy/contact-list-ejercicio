460e82e2ed893e3bbc03a7f36de9775af21106e2
rvarga09 <146484780+rvarga09@users.noreply.github.com>
2025-04-17 21:01:50 +0000

    contact list con fetch 2.0

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import ContactCard from "../components/ContactCard.jsx";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
@@ -29,16 +30,30 @@ export const Home = () => {
 
 	const { store, dispatch } = useGlobalReducer()
 
+	useEffect(() => {
+		fetch("https://playground.4geeks.com/contact/agendas/rafael/contacts")
+				.then((response) => {
+					return response.json()
+				 })
+				.then((data) => {
+					console.log(data)
+				})
+				.catch(() => { })
+	}, [])
 	return (
 		<div className="text-center mt-5">
 			<h1>Agenda</h1>
-			<div className= "d-flex flex-column align-items-center">
+			<div className="d-flex flex-column align-items-center">
 
-				
-				<ContactCard contact={contactList[0]} />
-				<ContactCard contact={contactList[1]} />
-				<ContactCard contact={contactList[2]} />
-				
+				{
+					contactList.map((contact) => {
+						return (
+							<ContactCard key={contact.id} contact={contact} />
+						);
+					})
+				}
+
+
 
 			</div>
 
