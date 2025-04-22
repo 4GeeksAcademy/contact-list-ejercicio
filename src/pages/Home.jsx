9f07e552f09478e778ae217c38efe829d0c46875
rvarga09 <146484780+rvarga09@users.noreply.github.com>
2025-04-22 21:01:36 +0000

    contact list3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,29 +3,7 @@ import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import ContactCard from "../components/ContactCard.jsx";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
-let contactList = [
-	{
-		id: 1,
-		name: "Rafael Vargas",
-		direction: "caracas, Venezuela",
-		phone: "0416-1234567",
-		email: "rafael@gmail.com"
-	},
-	{
-		id: 2,
-		name: "Maria Vargas",
-		direction: "Caracas, Venezuela",
-		phone: "0412-3456789",
-		email: "maria24@gmail.com"
-	},
-	{
-		id: 3,
-		name: "Jose Vargas",
-		direction: "Caracas, Venezuela",
-		phone: "0412-9876543",
-		email: "lucciano@gmail.com"
-	}
-];
+
 export const Home = () => {
 
 	const { store, dispatch } = useGlobalReducer()
